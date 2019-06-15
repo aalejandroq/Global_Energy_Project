fa288bc3cac5198726cc2ac6b818bffc89cff9b7
angelalejandro <45617025+aalejandroq@users.noreply.github.com>
2019-06-14 20:02:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -45,22 +45,18 @@ def home():
 
 @app.route("/countries")
 def countries():
-
     return jsonify(list(total_consumption_df.columns)[1:])
 
 @app.route("/total_consumption/<country>")
 def totalconsumption(country):
-
     data = {
         "year": total_consumption_df.Year.values.tolist(),
         "total_consumption": total_consumption_df[country].values.tolist(),
     }
     return jsonify(data)
 
-
 @app.route("/oil_consumption/<country>")
 def oilconsumption(country):
-
     data = {
         "year": oil_consumption_df.Year.values.tolist(),
         "oil_consumption": oil_consumption_df[country].values.tolist(),
@@ -69,27 +65,22 @@ def oilconsumption(country):
 
 @app.route("/ng_consumption/<country>")
 def ngconsumption(country):
-
     data = {
         "year": ng_consumption_df.Year.values.tolist(),
         "ng_consumption": ng_consumption_df[country].values.tolist(),
     }
     return jsonify(data)
 
-
 @app.route("/electricity_consumption/<country>")
 def electconsumption(country):
-
     data = {
         "year": electricity_consumption_df.Year.values.tolist(),
         "electricity_consumption": electricity_consumption_df[country].values.tolist(),
     }
     return jsonify(data)
 
-
 @app.route("/coal_consumption/<country>")
 def coalconsumption(country):
-
     data = {
         "year": coal_consumption_df.Year.values.tolist(),
         "coal_consumption": coal_consumption_df[country].values.tolist(),
@@ -98,17 +89,14 @@ def coalconsumption(country):
 
 @app.route("/electricity_renewables/<country>")
 def electrenewables(country):
-
     data = {
         "year": electricity_renewables_df.Year.values.tolist(),
         "electricity_renewables": electricity_renewables_df[country].values.tolist(),
     }
     return jsonify(data)
 
-
 @app.route("/emissions/<country>")
 def emissions(country):
-
     data = {
         "year": emissions_df.Year.values.tolist(),
         "emissions": emissions_df[country].values.tolist(),
@@ -117,7 +105,6 @@ def emissions(country):
 
 @app.route("/consumption/<country>")
 def consumption(country):
-
     data = {
         "year": total_consumption_df.Year.values.tolist(),
         "total_consumption": total_consumption_df[country].values.tolist(),
@@ -132,7 +119,6 @@ def consumption(country):
 # Prob need to improve the route so it it returns data for a give country or a given year
 @app.route("/gdp")
 def gdp():
-
     data = {
         "country": gdp_df.Country.values.tolist(),
         "year": gdp_df.Year.values.tolist(),
