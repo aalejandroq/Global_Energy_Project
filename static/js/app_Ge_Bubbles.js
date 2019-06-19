3e84b7e4fea4034185d62d9022895fdbb5b7c988
velmarini <vasquezvel@gmail.com>
2019-06-19 17:00:21 -0500

    responsive bubbles js&html mod

--- static/js/app_Ge_Bubbles.js
+++ static/js/app_Ge_Bubbles.js
@@ -2,6 +2,30 @@
 // Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv', function (err, data) {
 // Plotly.d3.csv('https://raw.githubusercontent.com/aalejandroq/cancer_test/master/db/dummydata.csv', function (err, data) {
 
+// MAKE THE PLOTS RESPONSIVE
+(function () {
+  var d3 = Plotly.d3;
+  var WIDTH_IN_PERCENT_OF_PARENT = 100,
+      HEIGHT_IN_PERCENT_OF_PARENT = 80;
+
+  var gd3 = d3.selectAll(".responsive-plot")
+      .style({
+          width: WIDTH_IN_PERCENT_OF_PARENT + '%',
+          'margin-left': (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + '%',
+
+          height: HEIGHT_IN_PERCENT_OF_PARENT + 'vh',
+          'margin-bottom': (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + 'vh'
+      });
+
+  var nodes_to_resize = gd3[0]; //not sure why but the goods are within a nested array
+  window.onresize = function () {
+      for (var i = 0; i < nodes_to_resize.length; i++) {
+          Plotly.Plots.resize(nodes_to_resize[i]);
+      }
+  };
+
+})();
+
 d3.json(`/bubble`).then(function(data) {
 console.log(data)
 //   Create a lookup table to sort and regroup the columns of data,
