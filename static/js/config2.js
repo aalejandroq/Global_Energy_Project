88e7c9ef7d8c77a28059a4cda03f2d5a6f836941
velmarini <vasquezvel@gmail.com>
2019-06-19 00:57:31 -0500

    map js update

---
+++ static/js/config2.js
@@ -0,0 +1,4 @@
+// API key
+// const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+
+const API_KEY = "pk.eyJ1IjoiandhbGlhZ2EiLCJhIjoiY2p3aDBwNHU3MWVmbzRjb3hseTBnbTY0ciJ9.K198xPXWTqrt3Oq97-URuQ";
